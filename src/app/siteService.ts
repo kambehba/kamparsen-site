710b72b93768d6e773517abb0f714dc5efdc830f
Kam Parsen <kamparsen@yahoo.com>
2019-06-19 18:18:51 -0400

    added comments

---
+++ src/app/siteService.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {TimeDateModel} from './TimeDateModel';
+import {Subject} from 'rxjs';
+
+@Injectable()
+export class SiteService{
+
+    public timeDateModel : Subject<TimeDateModel>;
+
+    constructor(private http:HttpClient){
+        this.timeDateModel = new Subject<TimeDateModel>();
+
+    }
+
+    GetDateTimeInfo()
+    {
+        this.http.get<TimeDateModel>('http://worldclockapi.com/api/json/est/now').subscribe(
+            data=>{this.timeDateModel.next(data);
+            
+            });
+
+    }
+
+    GetWeatherInfo()
+    {
+        this.http.get<TimeDateModel>('http://api.openweathermap.org/data/2.5/weather?q=London').subscribe(
+            data=>{console.log(data);
+            
+            }); //kghjhg
+    }
+
+}
