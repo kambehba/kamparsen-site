27038a49e2bbc5de052c050e3789e368f27aa3d6
Kam Parsen <kamparsen@yahoo.com>
2019-06-23 22:48:05 -0400

    major change - added Grid design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,6 +18,20 @@ export class AppComponent {
   private currentTemp : number;
   private currentTime : string;
 
+  private company : string;
+  private jobtitle : string;
+  private responsibalities : string;
+
+  private educationImage:string;
+  private camoziImages:string[] = ["../assets/i4.JPG","../assets/i5.JPG","../assets/i6.JPG","../assets/i7.JPG","../assets/i8.JPG","../assets/i9.JPG",];
+  private camozziImage :string;
+
+  private show1999 :boolean;
+  private show2002 :boolean;
+
+  private companyLogo: string;
+  private honeywellLogo: string;
+
   private allMonth : string[] = ["Jan","Feb","March","April","May","June","July","Aug","Sep","Oct","Nov","Dec"];
 
   constructor(private siteService: SiteService){
@@ -35,6 +49,8 @@ export class AppComponent {
     this.currentTime = timeData.currentDateTime.substring(11,16);
 
     this.siteService.GetWeatherInfo();
+
+    this.show1999 = false;
      
     });
 
@@ -48,8 +64,43 @@ ConvertKelvinToFarenhite(params:number) : number{
   return (params-273.15) * 1.8 + 32;
 }
 
+setComozziImages()
+{
+
+}
 
 
+
+ShowBachelorDegree(){ this.educationImage = "../assets/i2.JPG";}
+ShowSeniorDesign(){ this.educationImage = "../assets/i3.JPG";}
+ShowMasterDegree(){ this.educationImage = "../assets/i1.JPG";}
+
+
+Show1999(){ 
+  this.show2002 = false;
+   this.show1999 = true;
+  
+   this.company = "CAMOZZI , Pneumatic and Automation";
+   this.companyLogo="../assets/companyLogo.png";
+   this.jobtitle = "Electrical and Automation  Engineer";
+   this.responsibalities= "Electrical Panel design , PLC programming , Robotics and Automation."
+
+   this.camozziImage = this.camoziImages[0];
+
+}
+
+Show2002(){ 
+  this.show1999 = false;
+  this.show2002 = true;
+  
+  this.company = "Honeywell , Pneumatic and Automation";
+  this.honeywellLogo="../assets/honeywellLogo.png";
+  this.jobtitle = "Electrical and Automation  Engineer";
+  this.responsibalities= "Electrical Panel design , PLC programming , Robotics and Automation."
+
+  
+
+}
 
 
 }
