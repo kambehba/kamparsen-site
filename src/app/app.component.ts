f5629ce6b5df6f3d68825b3c2b4c2f84ba350d24
Kam Parsen <kamparsen@yahoo.com>
2019-08-02 14:58:11 -0400

    New Version Completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,14 +9,8 @@ import { SiteService } from './siteService';
 })
 export class AppComponent {
 
-  title = 'kamparsen-site';
+  title = 'Kam Parsen';
 
-  // private currentWeekDay: string;
-  // private currentMonth: string;
-  // private currentDay: string;
-  // private currentYear: string;
-  // private currentTemp: number;
-  // private currentTime: string;
 
   private currentDate: string;
   private currentTemp: number;
@@ -31,20 +25,29 @@ export class AppComponent {
   private camoziImages: string[] = ["../assets/i4.JPG", "../assets/i5.JPG", "../assets/i6.JPG", "../assets/i7.JPG", "../assets/i8.JPG", "../assets/i9.JPG"];
   private espnImages: string[] = ["../assets/i11.PNG", "../assets/i12.PNG"];
   private raytheonImages: string[] = ["../assets/sigma.JPG", "../assets/i10.JPG"];
-  private novaImages: string[] = ["../assets/nova.PNG","../assets/1.JPG", "../assets/2.JPG", "../assets/3.JPG","../assets/cfft-1.PNG","../assets/cfft-2.PNG","../assets/cfft-3.PNG"];
-  private interestImages: string[] = ["../assets/i20.JPG", "../assets/i21.JPG", "../assets/i22.JPG","../assets/i23.JPG"];
-  
+  private novaImages: string[] = ["../assets/nova.PNG", "../assets/1.JPG", "../assets/2.JPG", "../assets/3.JPG", "../assets/cfft-1.PNG", "../assets/cfft-2.PNG", "../assets/cfft-3.PNG"];
+  private interestImages: string[] = ["../assets/i20.JPG", "../assets/i21.JPG", "../assets/i22.JPG", "../assets/i23.JPG"];
+
   private camozziImage: string;
   private espnImage: string;
   private raytheonImage: string;
   private novaImage: string;
   private githubImage: string = "../assets/github.PNG";
   private interestImage: string;
-  private bsImage: string ="../assets/i2.JPG"
+  private bsImage: string = "../assets/i2.JPG"
 
-  
+
   private showEducationMaster: boolean;
   private showEducationBachelor: boolean;
+  private showEducationCertificate: boolean;
+
+  private showExperince1: boolean;
+  private showExperince2: boolean;
+  private showExperince3: boolean;
+  private showExperince4: boolean;
+  private showExperince5: boolean;
+  private showExperince6: boolean;
+
   private show1999: boolean;
   private show2002: boolean;
   private show2008: boolean;
@@ -54,11 +57,16 @@ export class AppComponent {
 
   private showNav: boolean;
   private showNav2: boolean;
+  private showNav3: boolean;
+  private showNav4: boolean;
+  private showNav5: boolean;
+
+  private buttonClicked : boolean;
+
 
   private showAbout: boolean;
-  private showEducation: boolean;
+ 
   private showExperience: boolean;
-  private showSkills: boolean;
   private showInterests: boolean;
   private showContact: boolean;
 
@@ -66,11 +74,15 @@ export class AppComponent {
   private showWebSkills: boolean;
   private showFirmwareSkills: boolean;
 
-  private showInterest1:boolean;
-  private showInterest2:boolean;
-  private showInterest3:boolean;
-  private showInterest4:boolean;
-  private showInterest5:boolean;
+  private showSkill1: boolean;
+  private showSkill2: boolean;
+  private showSkill3: boolean;
+
+  private showInterest1: boolean;
+  private showInterest2: boolean;
+  private showInterest3: boolean;
+  private showInterest4: boolean;
+  private showInterest5: boolean;
 
   private companyLogo: string;
 
@@ -79,28 +91,34 @@ export class AppComponent {
   private currentEspnImageIndex: number;
   private currentRaytheonImageIndex: number;
   private currentNovaImageIndex: number;
-  private currentInterestImageIndex : number;
+  private currentInterestImageIndex: number;
 
   private allMonth: string[] = ["Jan", "Feb", "March", "April", "May", "June", "July", "Aug", "Sep", "Oct", "Nov", "Dec"];
 
   constructor(private siteService: SiteService) {
-   // this.siteService.GetDateTimeInfo();
+    // this.siteService.GetDateTimeInfo();
 
 
   }
 
   ngOnInit() {
-    this.showEducationMaster = false;
-    this.showEducationBachelor = false;
+
+    this.hideAll();
+    this.hideAllNav();
+
+    this.buttonClicked = false;
+
+
     this.showNav = true;
-    this.showNav2 = false;
+    
+
     this.currentDate = new Date().toDateString();
     this.siteService.GetWeatherInfo();
     this.interestText = '';
     this.interestTitle = '';
-    this.hideAllExperinces();
+   
     this.hideAllInterests();
-    this.hideAllSkills();
+   
     this.currentCamozziImageIndex = 0;
     this.currentEspnImageIndex = 0;
     this.currentNovaImageIndex = 0;
@@ -114,9 +132,212 @@ export class AppComponent {
 
   }
 
-  backFromEducation(){
+  hideAll() {
+    this.showAbout = false;
+    this.showInterests = false;
+    this.showEducationMaster = false;
+    this.showEducationBachelor = false;
+    this.showEducationCertificate = false;
+    this.showExperince1 = false;
+    this.showExperince2 = false;
+    this.showExperince3 = false;
+    this.showExperince4 = false;
+    this.showExperince5 = false;
+    this.showSkill1 = false;
+    this.showSkill2 = false;
+    this.showSkill3 = false;
+    this.showInterest1 = false;
+    this.showInterest2 = false;
+    this.showInterest3 = false;
+    this.showInterest4 = false;
+    this.showContact = false;
+  }
+
+  hideAllNav()
+  {
+    this.showNav = false ;
+    this.showNav2 = false;
+    this.showNav3 = false;
+    this.showNav4 = false;
+    this.showNav5 = false;
+  }
+
+  ShowExperince(level: string) {
+    this.hideAll();
+
+
+    switch (level) {
+      case 'e1':
+        this.showExperince1 = true;
+        break;
+      case 'e2':
+        this.showExperince2 = true;
+        break;
+      case 'e3':
+        this.showExperince3 = true;
+        break;
+      case 'e4':
+        this.showExperince4 = true;
+        break;
+      case 'e5':
+        this.showExperince5 = true;
+        break;
+    }
+
+  }
+
+  showEducation(educationOption: string) {
+    this.hideAll();
+
+
+    switch (educationOption) {
+      case 'm': {
+        this.showEducationMaster = true;
+        this.buttonClicked = true;
+        break;
+      }
+
+      case 'b': {
+        this.showEducationBachelor = true;
+       
+        break;
+      }
+
+      case 'c': {
+        this.showEducationCertificate = true;
+        break;
+      }
+    }
+
+  }
+
+  Showskill(skillsOption: string) {
+
+    this.hideAll();
+    
+
+    switch (skillsOption) {
+      case 's1': {
+        this.showSkill1 = true;
+        break;
+      }
+
+      case 's2': {
+        this.showSkill2 = true;
+        break;
+      }
+
+      case 's3': {
+        this.showSkill3 = true;
+        break;
+      }
+    }
+  }
+
+
+  Showinterest(interestOption: string) {
+
+    this.hideAll();
+   
+
+    switch (interestOption) {
+      case 'i1': {
+        this.showInterest1 = true;
+        break;
+      }
+
+      case 'i2': {
+        this.showInterest2 = true;
+        break;
+      }
+
+      case 'i3': {
+        this.showInterest3 = true;
+        break;
+      }
+
+      case 'i4': {
+        this.showInterest4 = true;
+        break;
+      }
+    }
+
+
+  }
+
+  loadAbout() {
+   
+    this.showAbout = true;
+
+  }
+
+  loadEducation() {
+    this.hideAll();
+    this.showNav = false;
+    this.showNav2 = true;
+    this.showEducationMaster = true;
+    this.showAbout = false;
+
+  }
+
+
+
+  loadExperience() {
+    this.hideAll();
+    this.showNav3 = true;
+    this.showNav = false;
+    this.showNav2 = false;
+    this.showExperince1 = true;
+    this.showAbout = false;
+
+  }
+
+  loadSkills() {
+    this.hideAll();
+    this.hideAllNav();
+
+    this.showNav4 = true;
+    this.showSkill1 = true;
+    
+  }
+
+  loadInterests() {
+    this.hideAll();
+    this.hideAllNav();
+    this.showNav5 = true;
+    this,this.showInterest1 = true;
+  
+  }
+
+  loadContact() {
+    this.hideAll();
+    this.showContact = true;
+
+  }
+
+  hideAllInterests() {
+    this.showInterest1 = false;
+    this.showInterest2 = false;
+    this.showInterest3 = false;
+
+  }
+
+  back(option: string) {
+
+    this.hideAll();
+    this.hideAllNav();
+    this.showNav = true;
+    this.showAbout = true;
+  }
+
+  
+
+  backFromExperience() {
+    this.hideAll();
     this.showNav = true;
     this.showNav2 = false;
+    this.showNav3 = false;
+    this.showNav4 = false;
     this.showEducationMaster = false;
     this.showAbout = true;
   }
@@ -127,72 +348,11 @@ export class AppComponent {
   }
 
 
-
-  ShowBachelorDegree() { this.educationImage = "../assets/i2.JPG"; }
-  ShowSeniorDesign() { this.educationImage = "../assets/i3.JPG"; }
-  ShowMasterDegree() { this.educationImage = "../assets/i1.JPG"; }
-
-
-  Show1999() {
-    this.hideAllExperinces();
-    this.show1999 = true;
-
-    this.company = "CAMOZZI , Pneumatic and Automation";
-    this.companyLogo = "../assets/companyLogo.png";
-    this.jobtitle = "Electrical and Automation  Engineer";
-    this.responsibalities = "Electrical Panel design , PLC programming , Robotics and Automation."
-    this.camozziImage = this.camoziImages[this.currentCamozziImageIndex];
-
-  }
-
-  Show2002() {
-    this.hideAllExperinces();
-    this.show2002 = true;
-
-    this.company = "Honeywell";
-    this.companyLogo = "../assets/honeywellLogo.png";
-    this.jobtitle = "Electrical , BMS, BAS  Engineer";
-    
-  }
-
-  Show2008() {
-    this.hideAllExperinces();
-    this.show2008 = true;
-    this.raytheonImage = this.raytheonImages[this.currentRaytheonImageIndex];
-    this.company = "Raytheon";
-    this.companyLogo = "../assets/raytheonLogo.jpg";
-    this.jobtitle = "Software Engineer";
-    this.responsibalities = "Design and Impelemenation of Mission Managment System "
-
-  }
-
-  Show2010() {
-    this.hideAllExperinces();
-    this.show2010 = true;
-    this.espnImage = this.espnImages[this.currentEspnImageIndex];
-    this.company = "ESPN";
-    this.companyLogo = "../assets/espnLogo.png";
-    this.jobtitle = "Software Engineer";
-    this.responsibalities = "Design and Impelemenation of Mission Managment System "
-
-  }
-
-  Show2013() {
-    this.hideAllExperinces();
-    this.show2013 = true;
-    this.novaImage = this.novaImages[this.currentNovaImageIndex];
-    this.company = "Nova Techhologies";
-    this.companyLogo = "../assets/novaLogo.jpg";
-    this.jobtitle = "Software Engineer";
-    this.responsibalities = "Software Design and Impelemenation , CALL FOR FIRE (CFFT III) Trainer";
-
-  }
-
   ShowGithub() {
-    this.hideAllExperinces();
+    
     this.showGithub = true;
-  
-    this.githubImage =this.githubImage;
+
+    this.githubImage = this.githubImage;
     this.company = "Github";
     this.companyLogo = "../assets/GitHubLogo.PNG";
     this.jobtitle = "Software Engineer";
@@ -212,14 +372,11 @@ export class AppComponent {
 
     if (this.show2008) {
       this.currentRaytheonImageIndex--;
-     
+
       if (this.currentRaytheonImageIndex <= 0) this.currentRaytheonImageIndex = 1;
       else this.currentRaytheonImageIndex = 0;
 
       this.raytheonImage = this.raytheonImages[this.currentRaytheonImageIndex];
-
-     
-
     }
 
     if (this.show2010) {
@@ -265,7 +422,7 @@ export class AppComponent {
 
       this.espnImage = this.espnImages[this.currentEspnImageIndex];
     }
-    
+
     if (this.show2013) {
       this.currentNovaImageIndex++;
 
@@ -276,147 +433,24 @@ export class AppComponent {
 
   }
 
-  nextInterestImage(){
-    
-    if(this.showInterest1){ this.showInterest1 = false; this.showInterest2 = true;return;}
-    if(this.showInterest2) {this.showInterest2 = false; this.showInterest3 = true;return;}
-    if(this.showInterest3) {this.showInterest3 = false; this.showInterest4 = true;return;}
-    if(this.showInterest4) {this.showInterest4 = false; this.showInterest5 = true;return;}
-    if(this.showInterest5) {this.showInterest5 = false; this.showInterest1 = true;return;}
-   
-  }
+  nextInterestImage() {
 
-  prevInterestImage(){
-    
-    if(this.showInterest1){ this.showInterest1 = false; this.showInterest5 = true;return;}
-    if(this.showInterest2) {this.showInterest2 = false; this.showInterest1 = true;return;}
-    if(this.showInterest3) {this.showInterest3 = false; this.showInterest2 = true;return;}
-    if(this.showInterest4) {this.showInterest4 = false; this.showInterest3 = true;return;}
-    if(this.showInterest5) {this.showInterest5 = false; this.showInterest4 = true;return;}
-   
-  }
-
-  loadAbout() {
-    this.hideAllSections();
-    this.showAbout = true;
+    if (this.showInterest1) { this.showInterest1 = false; this.showInterest2 = true; return; }
+    if (this.showInterest2) { this.showInterest2 = false; this.showInterest3 = true; return; }
+    if (this.showInterest3) { this.showInterest3 = false; this.showInterest4 = true; return; }
+    if (this.showInterest4) { this.showInterest4 = false; this.showInterest5 = true; return; }
+    if (this.showInterest5) { this.showInterest5 = false; this.showInterest1 = true; return; }
 
   }
 
-  loadEducation() {
-    //this.hideAllSections();
-    this.showEducation = true;
-    this.bsImage = "../assets/i2.JPG"
-    this.showNav = false;
-    this.showNav2 = true;
-   
+  prevInterestImage() {
+
+    if (this.showInterest1) { this.showInterest1 = false; this.showInterest5 = true; return; }
+    if (this.showInterest2) { this.showInterest2 = false; this.showInterest1 = true; return; }
+    if (this.showInterest3) { this.showInterest3 = false; this.showInterest2 = true; return; }
+    if (this.showInterest4) { this.showInterest4 = false; this.showInterest3 = true; return; }
+    if (this.showInterest5) { this.showInterest5 = false; this.showInterest4 = true; return; }
 
   }
-
-  loadEducation2(educationOption : string) {
-    
-    this.hideAllSections();
-
-    switch(educationOption)
-    {
-      case 'm':{
-        this.showEducationMaster = true;
-        break;
-      }
-
-      case 'b':{
-        this.showEducationBachelor = true;
-        break;
-      }
-
-      case 'c':{
-        break;
-      }
-    }
-
-  }
-
-  loadExperience() {
-    this.hideAllSections();
-    this.showExperience = true;
-
-  }
-
-  loadSkills() {
-    this.hideAllSections();
-    this.hideAllExperinces();
-    this.hideAllSkills();
-    this.showSkills = true;
-    this.showDesktopSkills = true;
-
-  }
-
-  loadInterests() {
-    this.hideAllSections();
-    this.hideAllInterests();
-    this.showInterests = true;
-   
-    this.showInterest1 = true;
-  }
-
-  loadContact() {
-    this.hideAllSections();
-    this.showContact = true;
-
-  }
-
-  hideAllInterests(){
-    this.showInterest1 = false;
-    this.showInterest2 = false;
-    this.showInterest3 = false;
-
-  }
-
-  hideAllSections() {
-    this.showAbout = false;
-    this.showEducation = false;
-    this.showExperience = false;
-    this.showSkills = false;
-    this.showInterests = false;
-    this.showContact = false;
-    this.showEducationMaster = false;
-    this.showEducationBachelor = false;
-
-  }
-
-  hideAllExperinces() {
-    this.show1999 = false;
-    this.show2002 = false;
-    this.show2008 = false;
-    this.show2010 = false;
-    this.show2013 = false;
-    this.showGithub = false;
-  }
-
-  hideAllSkills() {
-    this.showDesktopSkills = false;
-    this.showWebSkills = false;
-    this.showFirmwareSkills = false;
-
-
-  }
-
-  ShowDesktopSkills() {
-    this.hideAllSkills();
-    this.showDesktopSkills = true;
-
-  }
-
-  ShowWebSkills() {
-    this.hideAllSkills();
-    this.showWebSkills = true;
-
-  }
-
-  ShowFirmwareSkills() {
-    this.hideAllSkills();
-    this.showFirmwareSkills = true;
-
-  }
-
 
 }
